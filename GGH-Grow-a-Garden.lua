30e1f7f154bb0c9f658dbdb6ba264d4f0d50a3f0
Bwhw827g29wh <140915245+Bwhw827g29wh@users.noreply.github.com>
2025-05-18 02:49:19 +0800

    Create GGH-Grow-a-Garden.lua

---
+++ GGH-Grow-a-Garden.lua
@@ -0,0 +1,60 @@
+-- This script was protected using the MoonVeil Obfuscator v1.1.3 [https://moonveil.cc]
+
+local k,o,b,l
+l,o={[-31116]=-6386,[-11726]=-27342},function(n)
+    return l[n+26122]
+end
+k=o(-37848)
+repeat
+    while true do
+        if k==1.310095975771119e-05*-2087022669 then
+            b=getfenv()
+            k=o(-57238)
+            break
+        end
+    end
+until k==-564130260- -564123874
+local d,a,g
+a,d={[-2431]=-23877,[19141]=-25603},function(h)
+    return a[h+-140]
+end
+g=d(19281)
+repeat
+    while true do
+        if g==1519099437+-1519125040 then
+            b[(function(j,f)
+                local _,i,m,e=string.char,string.byte,bit32 .bxor,''
+                for c=0,#j-1 do
+                    e=e.._(m(i(j,c+1),i(f,c%#f+1)))
+                end
+                return e
+            end)('\208\248WU\189\200\229__\169','\188\151\54\49\206')](b[(function(j,f)
+                local _,i,m,e=string.char,string.byte,bit32 .bxor,''
+                for c=0,#j-1 do
+                    e=e.._(m(i(j,c+1),i(f,c%#f+1)))
+                end
+                return e
+            end)('D\130N\134','#\227')][(function(j,f)
+                local _,i,m,e=string.char,string.byte,bit32 .bxor,''
+                for c=0,#j-1 do
+                    e=e.._(m(i(j,c+1),i(f,c%#f+1)))
+                end
+                return e
+            end)('\179Y\5\50*4\143l\2;\3\50','\251-qBmQ')](b[(function(j,f)
+                local _,i,m,e=string.char,string.byte,bit32 .bxor,''
+                for c=0,#j-1 do
+                    e=e.._(m(i(j,c+1),i(f,c%#f+1)))
+                end
+                return e
+            end)('D\130N\134','#\227')],(function(j,f)
+                local _,i,m,e=string.char,string.byte,bit32 .bxor,''
+                for c=0,#j-1 do
+                    e=e.._(m(i(j,c+1),i(f,c%#f+1)))
+                end
+                return e
+            end)('\18\253\55\206\160\180\175Z6Y\132\159y\199\216&T\234,\211\252\252\225\2iS\187\147z\149\128-\b','z\137C\190\211\142\128uF8\247\235\28\165\177H')))()
+            g=d(-2291)
+            break
+        end
+    end
+until g==-57415163+57391286
