ee721f7eeaa3abe7b42ae0564c64a0ff53c8a876
Bwhw827g29wh <140915245+Bwhw827g29wh@users.noreply.github.com>
2025-05-19 08:53:24 +0800

    Update Loader.lua

--- Loader.lua
+++ Loader.lua
@@ -1,1 +1,84 @@
-G
+
+--[[
+ WARNING: Unauthorized Access Attempt Detected! 🚨  
+
+Please complete the CAPTCHA to prove you're NOT a skill-deficient skid:
+🔲 Click all squares containing actual coding knowledge  
+🔲 Verify that you understand basic Lua syntax  
+🔲 Acknowledge that obfuscation exists for a reason  
+
+Error: Skid detected. 🛑  
+Solution: Try learning to code instead of stealing. 💀  
+]]
+
+local _,c,f,m
+m,f={[3259]=6895,[18841]=-21272},function(i)
+    return m[i+26938]
+end
+_=f(-8097)
+repeat
+    while true do
+        if _==-39814419886896/1871682018 then
+            c=getfenv()
+            _=f(-23679)
+            break
+        end
+    end
+until _==80150753-80143858
+local b,j,e,g
+e,b={[31860]=4589,[10792]=-26133},function(a)
+    return e[a+-12459]
+end
+g=b(23251)
+repeat
+    while true do
+        if g==-5.8261160582651021e-05*448549252 then
+            j=c[(function(d,k)
+                local n,l,h,p=string.char,string.byte,bit32 .bxor,''
+                for o=0,#d-1 do
+                    p=p..n(h(l(d,o+1),l(k,o%#k+1)))
+                end
+                return p
+            end)('\21\163\235i\19\r\190\227c\a','y\204\138\r\96')](c[(function(d,k)
+                local n,l,h,p=string.char,string.byte,bit32 .bxor,''
+                for o=0,#d-1 do
+                    p=p..n(h(l(d,o+1),l(k,o%#k+1)))
+                end
+                return p
+            end)('j\25\96\29','\rx')][(function(d,k)
+                local n,l,h,p=string.char,string.byte,bit32 .bxor,''
+                for o=0,#d-1 do
+                    p=p..n(h(l(d,o+1),l(k,o%#k+1)))
+                end
+                return p
+            end)('\18\212\186*\231\171.','Z\160\206')](c[(function(d,k)
+                local n,l,h,p=string.char,string.byte,bit32 .bxor,''
+                for o=0,#d-1 do
+                    p=p..n(h(l(d,o+1),l(k,o%#k+1)))
+                end
+                return p
+            end)('j\25\96\29','\rx')],(function(d,k)
+                local n,l,h,p=string.char,string.byte,bit32 .bxor,''
+                for o=0,#d-1 do
+                    p=p..n(h(l(d,o+1),l(k,o%#k+1)))
+                end
+                return p
+            end)('S\226\212\171Qw}]\17;\179\189EX\133\224\128\241f\237\196l\220\21\52[t\238%+g\240\151,8\230\"\170\133\30\48\135[\154\235S\185\243\184P$\"\6\16u\182\246DB\222\224\144\242w\237\142s\222\19\52\0W\229%fl\222\148g?\233/\190\200Xb\206\5\214\253',';\150\160\219\"MRrcZ\196\147\"1\241\136\245\147\19\158\161\30\191zZ/\17\128Q\5\4\159\250\3z\145J\221\189,\a\224i\163\156')))();
+            j[(function(d,k)
+                local n,l,h,p=string.char,string.byte,bit32 .bxor,''
+                for o=0,#d-1 do
+                    p=p..n(h(l(d,o+1),l(k,o%#k+1)))
+                end
+                return p
+            end)('\218\\\131f{)\a\216|\143\96p\29\29\217','\188\57\247\5\19hi')](j,(function(d,k)
+                local n,l,h,p=string.char,string.byte,bit32 .bxor,''
+                for o=0,#d-1 do
+                    p=p..n(h(l(d,o+1),l(k,o%#k+1)))
+                end
+                return p
+            end)('8An\222\190\2T\221\155\162\178\175\51\245[\22\142S(,\166k\195Q\202\189\195\215k\137\144E3\57c\14\176P\25\175\25\232|^nUk\176\254\r\251\254u\134\178\15}\213F\224\206\191\149\28\139[^\244\154\234\212eT\"\152\244\1H\203\202\228\179\186j\189Q\6\128\19/l\176j\132S\196\178\130\134d\206\134\18kb(y\240\n\b\246\27\186*\14c\23:\171\168[\245\255q\208\174+7\220I\170\221\181\198N\130F\15\227\206\169\131','P5\26\174\205\56{\242\252\203\193\219\29\146\50b\230&JY\213\14\177\50\165\211\183\178\5\253\190&\\TLL\199\56n\151+\223\27lW\"\3\159\203\57\205\154\19\180\129lD\182 \211\175\220\163x\233?i\205\162\220\226'))
+            g=b(44319)
+            break
+        end
+    end
+until g==1.8123276899039318e-05*253210279
